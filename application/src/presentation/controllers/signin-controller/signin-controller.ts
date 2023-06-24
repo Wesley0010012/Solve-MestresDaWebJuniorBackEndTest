183868ccb2c0391440366f7cd0b7d5ae0891384d
Wesley0010012 <wglaurindo33@gmail.com>
2023-06-24 16:40:30 -0300

    feat: Ensure return 400 if no email is provided

--- application/src/presentation/controllers/signin-controller/signin-controller.ts
+++ application/src/presentation/controllers/signin-controller/signin-controller.ts
@@ -1,10 +1,18 @@
+import { Controller } from "../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 
-export default class SigninController {
-  handle() {
-    return {
+export default class SigninController implements Controller {
+  handle(request: HttpRequest): HttpResponse {
+    if(!request.body.email)
+      return {
       statusCode: 400,
       body: 'Invalid email is provided'
-    }
+    };
+
+    return {
+      statusCode: 200,
+      body: 'Test Passed'
+    };
   }
 }
