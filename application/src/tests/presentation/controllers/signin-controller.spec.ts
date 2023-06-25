83c90ec4a78576fca11a827f9c6f2d68ec6ba7e3
Wesley0010012 <wglaurindo33@gmail.com>
2023-06-25 13:41:41 -0300

    refactor: change invalid-param-error to missing-param-error

--- application/src/tests/presentation/controllers/signin-controller.spec.ts
+++ application/src/tests/presentation/controllers/signin-controller.spec.ts
@@ -1,12 +1,13 @@
 import SigninController from "../../../presentation/controllers/signin-controller/signin-controller";
 import InvalidParamError from "../../../presentation/errors/invalid-param-error";
+import MissingParamError from "../../../presentation/errors/missing-param-error";
 import { HttpRequest } from "../../../presentation/protocols/http";
 
 describe('SignInController Tests', () => {
   test('Should Return 400 if no email is provided.', () => {
     const sut = new SigninController;
 
-    const error = new InvalidParamError('email');
+    const error = new MissingParamError('email');
 
     const request: HttpRequest = {
       body: {
@@ -25,7 +26,7 @@ describe('SignInController Tests', () => {
   test('Should Return 400 if no password is provided.', () => {
     const sut = new SigninController;
 
-    const error = new InvalidParamError('password');
+    const error = new MissingParamError('password');
 
     const request: HttpRequest = {
       body: {
