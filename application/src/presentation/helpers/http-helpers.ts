bb7379ff1bc174e06f07d0a454086ea225564771
Wesley0010012 <wglaurindo33@gmail.com>
2023-06-25 12:59:12 -0300

    refactor: create server-error and success body

--- application/src/presentation/helpers/http-helpers.ts
+++ application/src/presentation/helpers/http-helpers.ts
@@ -5,3 +5,13 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 });
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+});
+
+export const success = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+});
