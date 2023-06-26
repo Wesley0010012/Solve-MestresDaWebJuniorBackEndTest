4b70ad6ecd29a914e8e21c7e6cda8b4e8a50a3d2
Wesley0010012 <wglaurindo33@gmail.com>
2023-06-26 15:55:25 -0300

    test: ensure encrypt sign have called with correct values

---
+++ application/tests/infra/criptography/jwt-adapter.spec.ts
@@ -0,0 +1,38 @@
+
+
+import jwt from 'jsonwebtoken';
+import { JWTAdapter } from '../../../src/infra/criptography/jwt-adapter';
+
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return 'any_token';
+  },
+
+  async encrypt(): Promise<string> {
+    return 'any_value';
+  }
+}));
+
+interface SutTypes {
+  sut: JWTAdapter;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new JWTAdapter('secret');
+
+  return {
+    sut
+  };
+}
+
+describe('JWTAdapter tests', () => {
+  test('Should Called Encrypt Sign with Correct Values', async () => {
+    const { sut } = makeSut();
+
+    const jwtSignSpy = jest.spyOn(jwt, 'sign');
+
+    await sut.encrypt('any_value');
+
+    expect(jwtSignSpy).toBeCalledWith({id: 'any_value'}, 'secret');
+  });
+})
