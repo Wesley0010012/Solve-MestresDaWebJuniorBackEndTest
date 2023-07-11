e941d41417d810f5ebaf2636a290260445651d95
Wesley0010012 <wglaurindo33@gmail.com>
2023-07-10 21:33:47 -0300

    feat: ensure return 401 if Decrypter throws

--- application/src/presentation/controllers/list-films-controller/list-films-controller.ts
+++ application/src/presentation/controllers/list-films-controller/list-films-controller.ts
@@ -1,17 +1,27 @@
-import { MissingParamError } from "../../errors";
-import { badRequest, success } from "../../helpers";
+
+import { Decrypter } from "../../../data";
+import { AccessDeniedError, MissingParamError } from "../../errors";
+import { badRequest, serverError, success, unauthorized } from "../../helpers";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 
 export class ListFilmsController implements Controller {
+  constructor(private readonly decrypter: Decrypter) {}
+
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const inputs = ['token', 'order'];
+    try {
+      const inputs = ['token', 'order'];
 
-    for(const input of inputs) {
-      if(!request.body[input])
-        return badRequest(new MissingParamError(input));
+      for(const input of inputs) {
+        if(!request.body[input])
+          return badRequest(new MissingParamError(input));
+      }
+
+      const { token, order } = request.body;
+
+      return success(this.decrypter.decrypt(token));
+    } catch(e: any) {
+      return unauthorized(new AccessDeniedError());
     }
-
-    return success("Passed Everything");
   }
 }
